41ff65f983642365875e3535aec2016629f3bb39
Aneuris99 <136648820+Aneuris99@users.noreply.github.com>
2023-06-21 19:59:11 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int numero = 10;
-Console.WriteLine($"El numero es {numero}");
+string nombre = "Cristian";
